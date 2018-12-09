7b5cce8b73cc63aa055f51db44a3c47709b104bd
Ender <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2018-12-09 18:46:26 +1300

    Update buildArch.sh

--- scripts/buildArch.sh
+++ scripts/buildArch.sh
@@ -1,20 +1,2 @@
-#! /bin/bash
 
-export ARCH_DIR=output/${1}
-export ROOTFS_DIR=$ARCH_DIR/rootfs
-
-case "$1" in
-    arm) export DEBOOTSTRAP_ARCH=armhf
-        ;;
-    arm64) export DEBOOTSTRAP_ARCH=arm64
-        ;;
-    x86) export DEBOOTSTRAP_ARCH=i386
-        ;;
-    x86_64) export DEBOOTSTRAP_ARCH=amd64
-        ;;
-    all) exit
-        ;;
-    *) echo "unsupported arch"
-        exit
-        ;;
  
