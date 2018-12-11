3a43b73e4cc71cef9263e78f8ccd1fbea7f2ec68
Ender <31677287+EnderNightLord-ChromeBook@users.noreply.github.com>
2018-12-11 19:51:33 +1300

    Update buildArch.sh

--- scripts/buildArch.sh
+++ scripts/buildArch.sh
@@ -1,31 +1,31 @@
 #! /bin/bash
 
-MIRROR=http://dl-5.alpinelinux.org/alpine
-ARCH=x86_64
-CHROOT=alpine-chroot-newest
-VERSION=latest-stable
-APK_TOOL=apk-tools-static-2.10.1-r0.apk
-
 export ARCH_DIR=output/${1}
 export ROOTFS_DIR=$ARCH_DIR/rootfs
 
 case "$1" in
-    armhf) export ARCH_BOOTSTRAP_ARCH_OPT=armv7h
+   armhf) export ARCH_BOOTSTRAP_ARCH_OPT=armv7h
         export ARCH_BOOTSTRAP_QEMU_OPT=-q
         ;;
-#   arm64) export ARCH_BOOTSTRAP_ARCH_OPT=aarch64
-#      export ARCH_BOOTSTRAP_QEMU_OPT=-q
-#       ;;
-#    x86) export ARCH_BOOTSTRAP_ARCH_OPT=i386
-#       ;;
-    x86_64) export ARCH_BOOTSTRAP_ARCH_OPT=x86_64
-        ;;
-    all) exit
-        ;;
-    *) echo "unsupported arch"
-        exit
-        ;;
+   arm64) export ARCH_BOOTSTRAP_ARCH_OPT=aarch64
+      export ARCH_BOOTSTRAP_QEMU_OPT=-q
+      ;;
+   x86) export ARCH_BOOTSTRAP_ARCH_OPT=i386
+   ;;
+   x86_64) export ARCH_BOOTSTRAP_ARCH_OPT=x86_64
+   ;;
+   all) exit
+   ;;
+   *) echo "unsupported arch"
+   exit
+   ;;
 esac
+
+MIRROR=http://dl-5.alpinelinux.org/alpine
+ARCH=$1
+CHROOT=alpine-chroot-newest
+VERSION=latest-stable
+APK_TOOL=apk-tools-static-2.10.1-r0.apk
 
 rm -rf $ARCH_DIR
 mkdir -p $ARCH_DIR
@@ -37,6 +37,7 @@ if [ ${MACHINE_TYPE} == 'x86_64' ]; then
 wget $MIRROR/$VERSION/main/x86_64/$APK_TOOL
 else
 wget $MIRROR/$VERSION/main/armhf/$APK_TOOL
+
 fi
 
 tar -xzf $APK_TOOL
