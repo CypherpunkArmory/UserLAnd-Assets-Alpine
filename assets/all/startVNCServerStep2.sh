bb8e2445c5c3ee46c0ccf4306a766fc21b50d31a
Corbin Champion <corbinlc@gmail.com>
2019-02-11 09:22:56 -0800

    tweak starting resolution

--- assets/all/startVNCServerStep2.sh
+++ assets/all/startVNCServerStep2.sh
@@ -14,7 +14,7 @@ x11vnc -storepasswd $INITIAL_VNC_PASSWORD /home/$INITIAL_USERNAME/.vnc/passwd
 fi
 
 rm /tmp/.X51-lock
-Xvfb :51 -screen 0 800x640x24 &
+Xvfb :51 -screen 0 1024x768x16 &
 sleep 1
 export DISPLAY=:51
 LANG=C twm &
