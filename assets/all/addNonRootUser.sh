b685036b8914077b2add0cf0d71de5570cb64f25
Corbin Champion <corbinlc@gmail.com>
2019-06-03 15:13:05 -0700

    add mail spool directory

--- assets/all/addNonRootUser.sh
+++ assets/all/addNonRootUser.sh
@@ -11,6 +11,7 @@ if [[ -z "${INITIAL_VNC_PASSWORD}" ]]; then
 fi
 
 if [ ! -d "/home/$INITIAL_USERNAME" ]; then
+  mkdir -p /var/spool/mail
   useradd $INITIAL_USERNAME -s /bin/bash -m -u 2000 
   echo $INITIAL_USERNAME:$INITIAL_PASSWORD | chpasswd
   echo "/bin/bash" >> /etc/shells
